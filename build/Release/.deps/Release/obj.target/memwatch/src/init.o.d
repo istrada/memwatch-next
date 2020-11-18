ec1330d348840d9075244389b3804fd9fbfedfa7
casterama <kwarda@sysdynecorp.com>
2020-11-18 13:31:27 +0100

    Initial commit.

---
+++ build/Release/.deps/Release/obj.target/memwatch/src/init.o.d
@@ -0,0 +1,62 @@
+cmd_Release/obj.target/memwatch/src/init.o := g++ '-DNODE_GYP_MODULE_NAME=memwatch' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/casterama/.node-gyp/8.9.4/include/node -I/home/casterama/.node-gyp/8.9.4/src -I/home/casterama/.node-gyp/8.9.4/deps/uv/include -I/home/casterama/.node-gyp/8.9.4/deps/v8/include -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/memwatch/src/init.o.d.raw   -c -o Release/obj.target/memwatch/src/init.o ../src/init.cc
+Release/obj.target/memwatch/src/init.o: ../src/init.cc \
+ /home/casterama/.node-gyp/8.9.4/include/node/v8.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/v8-version.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/v8config.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/v8.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node_version.h \
+ ../src/heapdiff.hh \
+ /home/casterama/.node-gyp/8.9.4/include/node/v8-profiler.h \
+ ../../nan/nan.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node_version.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv-errno.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv-version.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv-unix.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv-threadpool.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/uv-linux.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node_buffer.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node.h \
+ /home/casterama/.node-gyp/8.9.4/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h ../../nan/nan_private.h \
+ ../../nan/nan_typedarray_contents.h ../../nan/nan_json.h \
+ ../src/memwatch.hh
+../src/init.cc:
+/home/casterama/.node-gyp/8.9.4/include/node/v8.h:
+/home/casterama/.node-gyp/8.9.4/include/node/v8-version.h:
+/home/casterama/.node-gyp/8.9.4/include/node/v8config.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node.h:
+/home/casterama/.node-gyp/8.9.4/include/node/v8.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node_version.h:
+../src/heapdiff.hh:
+/home/casterama/.node-gyp/8.9.4/include/node/v8-profiler.h:
+../../nan/nan.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node_version.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv-errno.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv-version.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv-unix.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv-threadpool.h:
+/home/casterama/.node-gyp/8.9.4/include/node/uv-linux.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node_buffer.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node.h:
+/home/casterama/.node-gyp/8.9.4/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_private.h:
+../../nan/nan_typedarray_contents.h:
+../../nan/nan_json.h:
+../src/memwatch.hh:
---
+++ build/Release/.deps/Release/obj.target/memwatch/src/util.o.d
@@ -0,0 +1,6 @@
+cmd_Release/obj.target/memwatch/src/util.o := g++ '-DNODE_GYP_MODULE_NAME=memwatch' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/casterama/.node-gyp/8.9.4/include/node -I/home/casterama/.node-gyp/8.9.4/src -I/home/casterama/.node-gyp/8.9.4/deps/uv/include -I/home/casterama/.node-gyp/8.9.4/deps/v8/include -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/memwatch/src/util.o.d.raw   -c -o Release/obj.target/memwatch/src/util.o ../src/util.cc
+Release/obj.target/memwatch/src/util.o: ../src/util.cc \
+ ../src/platformcompat.hh ../src/util.hh
+../src/util.cc:
+../src/platformcompat.hh:
+../src/util.hh:
